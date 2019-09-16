d6a67fc81c26cf12662569ba1ad30a4b09561821
konstantindenerz <konstantin.denerz@gmail.com>
2019-09-16 13:49:50 +0200

    refactor(workspace): rename projects

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {CoreModule} from '@labs/common';
+import {CoreModule} from '../../../common';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
